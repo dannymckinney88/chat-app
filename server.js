25d71bad023a050d078fd058142eb2e0e94b3890
dannymckinney88 <dannymckinney88@gmail.com>
2021-02-11 22:54:23 -0700

    init

--- server.js
+++ server.js
@@ -5,4 +5,18 @@ const server = http.createServer(app);
 const socket = require("socket.io");
 const io = socket(server);
 
+// Starts a new connection
+// -- takes a socket as an argument
+io.on("connection", (socket) => {
+  // First arrgument is any string abbitray
+  // Every socket gets a unqiue id so we know who is the sender and receiver
+  socket.emit("your id", socket.id);
+  // Every time a user sends a mseesage .on listens for message
+  // Body contains the meesage
+  socket.on("send meesage", (body) => {
+    // io emmit sends out to all people connected
+    io.emit("message", body);
+  });
+});
+
 server.listen(8000, () => consol.log("Server is running on port 8000"));
